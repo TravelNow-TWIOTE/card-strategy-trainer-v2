9ee2e06a79473d857634cca0cd3ea50af91a27d0
TravelNow-TWIOTE <nbochner28@sarhighschool.org>
2026-03-13 08:38:33 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,37 +1,38 @@
-let startingPoints = 1000
+let startingPoints=1000
+let points=1000
+let wager=0
 
-let points = 1000
-let wager = 0
+let rounds=0
+let wins=0
+let losses=0
+let reshuffles=0
 
-let rounds = 0
-let wins = 0
-let losses = 0
+let deck=[]
+let dealer=[]
 
-let deck = []
-let dealer = []
+let hands=[[]]
+let activeHand=0
 
-let hands = [[]]
-let activeHand = 0
-
-let active = false
+let active=false
 
 function update(){
 
-document.getElementById("points").innerText = "Points: " + points
+document.getElementById("points").innerText="Points: "+points
 
-document.getElementById("rounds").innerText = rounds
-document.getElementById("wins").innerText = wins
-document.getElementById("losses").innerText = losses
-document.getElementById("profit").innerText = points-startingPoints
+document.getElementById("rounds").innerText=rounds
+document.getElementById("wins").innerText=wins
+document.getElementById("losses").innerText=losses
+document.getElementById("reshuffles").innerText=reshuffles
+document.getElementById("profit").innerText=points-startingPoints
 
 }
 
 function deckBuild(){
 
-deck = []
+deck=[]
 
-let suits = ["S","H","D","C"]
-let ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+let suits=["S","H","D","C"]
+let ranks=["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
 
 for(let s of suits){
 for(let r of ranks){
@@ -41,10 +42,20 @@ deck.push({r:r,s:s})
 
 deck.sort(()=>Math.random()-0.5)
 
+reshuffles++
+
+}
+
+function autoShuffle(){
+
+if(deck.length<15){
+deckBuild()
+}
+
 }
 
 function cardImage(c){
-return "https://deckofcardsapi.com/static/img/" + c.r + c.s + ".png"
+return "https://deckofcardsapi.com/static/img/"+c.r+c.s+".png"
 }
 
 function value(c){
@@ -57,16 +68,16 @@ return parseInt(c.r)
 
 function total(hand){
 
-let t = 0
-let aces = 0
+let t=0
+let aces=0
 
 for(let c of hand){
-t += value(c)
+t+=value(c)
 if(c.r==="A") aces++
 }
 
 while(t>21 && aces>0){
-t -= 10
+t-=10
 aces--
 }
 
@@ -75,6 +86,7 @@ return t
 }
 
 function draw(){
+autoShuffle()
 return deck.pop()
 }
 
@@ -82,6 +94,7 @@ function render(){
 
 renderDealer()
 renderHands()
+
 update()
 
 }
@@ -100,7 +113,7 @@ document.getElementById("dealerTotal").innerText="Total: ?"
 }else{
 
 for(let c of dealer){
-html+="<img class='flip' src='"+cardImage(c)+"'>"
+html+="<img src='"+cardImage(c)+"'>"
 }
 
 document.getElementById("dealerTotal").innerText="Total: "+total(dealer)
@@ -114,24 +127,38 @@ document.getElementById("dealer").innerHTML=html
 function renderHands(){
 
 let h1=""
-
 for(let c of hands[0]){
 h1+="<img src='"+cardImage(c)+"'>"
 }
 
-document.getElementById("hand1").innerHTML=h1
+let hand1=document.getElementById("hand1")
+
+hand1.innerHTML=h1
 document.getElementById("total1").innerText="Total: "+total(hands[0])
+
+hand1.classList.remove("active-hand")
+
+if(activeHand===0 && active){
+hand1.classList.add("active-hand")
+}
 
 if(hands.length>1){
 
 let h2=""
-
 for(let c of hands[1]){
 h2+="<img src='"+cardImage(c)+"'>"
 }
 
-document.getElementById("hand2").innerHTML=h2
+let hand2=document.getElementById("hand2")
+
+hand2.innerHTML=h2
 document.getElementById("total2").innerText="Total: "+total(hands[1])
+
+hand2.classList.remove("active-hand")
+
+if(activeHand===1 && active){
+hand2.classList.add("active-hand")
+}
 
 }
 
@@ -147,8 +174,6 @@ if(wager>points){
 document.getElementById("msg").innerText="Not enough points"
 return
 }
-
-deckBuild()
 
 dealer=[draw(),draw()]
 hands=[[draw(),draw()]]
@@ -188,11 +213,12 @@ stand()
 
 function stand(){
 
+setTimeout(()=>{
 while(total(dealer)<17){
 dealer.push(draw())
 }
-
 finish()
+},600)
 
 }
 
@@ -227,6 +253,14 @@ render()
 
 }
 
+function showFloat(text,color){
+
+let m=document.getElementById("msg")
+
+m.innerHTML="<span class='float' style='color:"+color+"'>"+text+"</span>"
+
+}
+
 function finish(){
 
 active=false
@@ -240,17 +274,20 @@ let p=total(h)
 if(p>21){
 points-=wager
 losses++
+showFloat("-"+wager,"red")
 }
 else if(dealerTotal>21||p>dealerTotal){
 points+=wager
 wins++
+showFloat("+"+wager,"lightgreen")
 }
 else if(p<dealerTotal){
 points-=wager
 losses++
+showFloat("-"+wager,"red")
 }
 else{
-wins++
+showFloat("Push","white")
 }
 
 }
